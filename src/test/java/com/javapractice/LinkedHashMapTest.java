c91badae387584aa4f975f299043cef6eb2d724e
SRC0747 <RjSrc@0747>
2021-08-24 20:57:27 +0530

    added: Finding Frequency of word using LinkedList Hash Code.

--- src/test/java/com/javapractice/LinkedHashMapTest.java
+++ src/test/java/com/javapractice/LinkedHashMapTest.java
@@ -44,13 +44,32 @@ public class MyMapNode<K,V> implements INode<k> {
     }
 }
 
-class LinkedHashMapTest {
-    public class MyHashMap<k,v> {
-        LinkedList<k> myLinkedList;
-        public MyHashMap() {
-            this.myLinkedList = new LinkedList<>();
-        }
+public class LinkedHashMap<k,v> {
+    private final int numBucket;
+    ArrayList<LinkedList<k>> myBucketArray;
 
-        }
+    public LinkedHashMap() {
+        this.numBucket = 10;
+        this.myBucketArray = new ArrayList<>(numBucket);
+        //Create Empty Linked List
+        for (int i = 0; i< numBucket; i++)
+            this.myBucketArray.add(null);
+    }
+    //This implements hash to find index for a key.
+    private int getBucketIndex(k key)
+    {
+        int hashCode = Math.abs(key.hashCode());
+        int index = hashCode%numBucket;
+        return index;
+    }
+    public V get(K key)
+    {
+        int index = this.getBucketIndex(key);
+        LinkedList<K> myLinkedList = this.myBucketArray.get(index);
+        if (myLinkedList==null)
+            return null;
+        MyMapNode<K,V> myMapNode = (MyMapNode<K,V>) myLinkedList.search(key);
+        return (myMapNode == null) ? null:myMapNode.getValue();
     }
 }
+
